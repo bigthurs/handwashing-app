a08e019996c0e2400082c75eab2549ce07c32ec8
Trevor Hursey <trevorhursey@yahoo.com>
2021-04-13 15:31:43 -0700

    got the timer code done

--- src/ProgressBar.svelte
+++ src/ProgressBar.svelte
@@ -1,18 +1,22 @@
+<script>
+    export let progress;
+</script>
+
+<style>
+    .progress-container {
+      margin: 20px 0;
+      border: solid rgb(194, 194, 194) 1px;
+    }
+    .progress-bar {
+      height: 30px;
+      background-color: rgb(77, 199, 55);
+    }
+</style>
+
 <div bp="grid">
   <div bp="offset-5@md 4@md 12@sm" class="progress-container">
-    <div style="width:20%;" class="progress-bar">
-      <span class="sr-only">%</span>
+    <div style="width:{progress}%;" class="progress-bar">
+      <span class="sr-only">%{progress}</span>
     </div>
   </div>
 </div>
-
-<style>
-  .progress-container {
-    margin: 20px 0;
-    border: solid rgb(194, 194, 194) 1px;
-  }
-  .progress-bar {
-    height: 30px;
-    background-color: rgb(77, 199, 55);
-  }
-</style>
--- src/Timer.svelte
+++ src/Timer.svelte
@@ -1,14 +1,37 @@
 <script>
   import ProgressBar from "./ProgressBar.svelte";
+
+  const totalSeconds = 20;
+  let secondsLeft = totalSeconds;
+  let isRunning = false;
+  $: progress = ((totalSeconds - secondsLeft) / totalSeconds) * 100;
+
+  const resetTimer = () => {
+    isRunning = true;
+    const timer = setInterval(() => {
+      secondsLeft--;
+      if (secondsLeft == 0) {
+        clearInterval(timer);
+        isRunning = false;
+        secondsLeft = totalSeconds;
+      }
+    }, 1000);
+  };
 </script>
 
 <div bp="grid">
-  <h2 bp="offset-5@md 4@md 12@sm">Seconds Left:</h2>
+  <h2 bp="offset-5@md 4@md 12@sm">Seconds Left:{secondsLeft}</h2>
 </div>
+<ProgressBar {progress} />
 
-<ProgressBar />
-
-<button class="start">Start</button>
+<div bp="grid">
+  <button
+    disabled={isRunning}
+    on:click={resetTimer}
+    bp="offset-5@md 4@md 12@sm"
+    class="start">Start</button
+  >
+</div>
 
 <style>
   h2 {
@@ -18,5 +41,9 @@
     background-color: rgb(154, 73, 73);
     width: 100%;
     margin: 10px 0;
+  }
+  .start[disabled] {
+    background-color: rgb(194, 194, 194);
+    cursor: not-allowed;
   }
 </style>
